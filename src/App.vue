f73e27951ede1b28b1ba13780ccb0e92c2afd0d9
korpusovmaxim858 <korpusovmax@gmail.com>
2023-05-29 20:27:09 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="colors">
+    <div class="primary"><p class="text">Primary</p></div>
+    <div class="primary_container"><p class="text">Primary container</p></div>
+  </div>
+</template>
+
+<style lang="scss">
+.colors {
+  width: 100vw;
+  height: 128px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.primary {
+  background-color: var(--primary-color);
+  color: var(--on-primary-color);
+}
+.primary_container {
+  background-color: var(--primary-container-color);
+  color: var(--on-primary-container-color);
+}
+.text {
+  font-size: 36px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    123123
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
