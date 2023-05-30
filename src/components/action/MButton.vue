245cd4e71ab8b8d03d12bbb195eff63f7735da86
korpusovmaxim858 <korpusovmax@gmail.com>
2023-05-31 02:35:17 +0300

    buttons with icon

--- src/components/action/MButton.vue
+++ src/components/action/MButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="btn" class="container" :class="[{'container--enabled':enabled}, type]">
-    <p class="container__label-text m-text m-headline-small">Label</p>
+    <p class="container__label-text m-text m-headline-small"><slot></slot></p>
   </div>
 </template>
 
@@ -23,41 +23,15 @@ export default {
   setup() {
 
   },
-  mounted() {
-    const createRipple = function (event) {
-      const button = event.currentTarget;
-
-      const circle = document.createElement("span");
-      const diameter = Math.max(button.clientWidth, button.clientHeight);
-      const radius = diameter / 2;
-
-      circle.style.width = circle.style.height = `${diameter}px`;
-      circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-      circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-      circle.classList.add("ripple");
-
-      const ripple = button.getElementsByClassName("ripple")[0];
-
-      if (ripple) {
-        ripple.remove();
-      }
-
-      button.appendChild(circle);
-    }
-    //Если кнопка активна - добавляем эффект нажатия
-    if (this.enabled) this.$refs.btn.addEventListener('click', createRipple);
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  padding: 10px 24px;
+  padding: 10px 24px 10px 16px;
   height: min-content;
-  width: min-content;
+  min-width: min-content;
 
-  background-color: var(--primary-color);
-  color: var(--on-primary-color);
   border-radius: 100px;
 
   cursor: pointer;
@@ -65,23 +39,46 @@ export default {
   position: relative;
   overflow: hidden;
   &__label-text {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin-left: 8px;
     user-select: none;
+
+    &:has(span) {
+      margin-left: 0;
+    }
   }
 
   &:not(&--enabled) {
     //opacity: 18%;
+    cursor: default;
     background-color: var(--primary-container-color-a18);
-    color: black;
+    color: var(--on-surface-light);
     .container__label-text {
-      //color - on surface TODO
-
       opacity: 38%;
     }
   }
-  &--enabled {
-    &:hover {
-      box-shadow: var(--elevation-light-1);
+  &--enabled:not(.outlined) {
+    &:hover:not(&:active) {
+      box-shadow: var(--elevation-1-light);
     }
+  }
+}
+
+.filled {
+  background-color: var(--primary-color);
+  color: var(--on-primary-color);
+}
+.filled-tonal {
+  background-color: var(--secondary-container-color);
+  color: var(--on-secondary-container-color);
+}
+.outlined {
+  color: var(--primary-color);
+  border: 1px solid var(--outline-light);
+  &:hover:not(&:active) {
+    background-color: var(--primary-color-a8);
   }
 }
 </style>
