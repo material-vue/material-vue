43facbb6104fb7e64348c798411c8cf0d26bc95d
korpusovmaxim858 <korpusovmax@gmail.com>
2023-05-31 02:34:51 +0300

    top bar init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
+  <m-top-bar/>
   <div class="colors">
     <div class="primary"><p class="m-text m-display-small">Primary</p></div>
     <div class="primary_container"><p class="m-text m-display-small">Primary container</p></div>
+    <div class="secondary"><p class="m-text m-display-small">Secondary</p></div>
+    <div class="secondary_container"><p class="m-text m-display-small">Secondary container</p></div>
   </div>
-  <m-button/>
-  <m-button :enabled="false"/>
+  <div class="buttons">
+    <m-button>Filled</m-button>
+    <m-button :enabled="false">Disabled</m-button>
+    <m-button type="filled-tonal">Filled tonal</m-button>
+    <m-button type="outlined">Outlined</m-button>
+  </div>
 </template>
 <script>
 import MButton from "@/components/action/MButton.vue";
+import MTopBar from "@/components/navigation/MTopBar.vue";
 export default {
-  components: {MButton}
+  components: {MTopBar, MButton}
 }
 </script>
 <style lang="scss">
 .colors {
   width: 100vw;
-  height: 128px;
+  height: 128*6px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   &>* {
@@ -32,7 +40,20 @@ export default {
   background-color: var(--primary-container-color);
   color: var(--on-primary-container-color);
 }
+.secondary {
+  background-color: var(--secondary-color);
+  color: var(--on-secondary-color);
+}
+.secondary_container {
+  background-color: var(--secondary-container-color);
+  color: var(--on-secondary-container-color);
+}
+
+.buttons {
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+  width: 100%;
+  margin-top: 16px;
+}
 </style>
-<script setup>
-import MButton from "@/components/action/MButton.vue";
-</script>
---
+++ src/components/navigation/MTopBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="top_bar" class="container">
+    <p class="m-text m-display-medium">Title</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let top_bar = this.$refs.top_bar;
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        top_bar.classList.add('container__scrolled');
+      } else {
+        top_bar.classList.remove('container__scrolled');
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: var(--surface-light);
+
+  transition: 0.4s;
+  position: fixed; // Sticky/fixed navbar
+  width: 100%;
+  top: 0;
+  padding: 16px;
+
+  &__scrolled {
+    padding: 8px 16px;
+  }
+}
+</style>
