e39f8b257d304feb658fec51f7323880ea194f03
korpusovmaxim858 <korpusovmax@gmail.com>
2023-06-02 01:16:25 +0300

    divider component added

---
+++ src/components/utils/MDivider.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="divider">
+
+  </div>
+</template>
+<style lang="scss" scoped>
+.divider {
+  width: 100%;
+  min-height: 1px;
+  background: var(--outline-var);
+}
+</style>
