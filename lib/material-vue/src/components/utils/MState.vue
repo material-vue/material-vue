b5398c522da94771dd015496bb95d42d99b4887a
korpusovmaxim858 <korpusovmax@gmail.com>
2023-07-11 18:23:16 +0300

    new state layer system

---
+++ lib/material-vue/src/components/utils/MState.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-state" ref="stateLayer">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+let stateLayer = ref<null | HTMLElement>(null)
+
+onMounted(() => {
+  stateLayer.value?.addEventListener('mouseenter', function() {
+    stateLayer.value?.classList.add('m-state--hover');
+  });
+
+  stateLayer.value?.addEventListener('mouseleave', function() {
+    stateLayer.value?.classList.remove('m-state--hover');
+  });
+
+  stateLayer.value?.addEventListener('touchstart', function() {
+    stateLayer.value?.classList.add('m-state--hover');
+  });
+
+  stateLayer.value?.addEventListener('touchend', function() {
+    stateLayer.value?.classList.remove('m-state--hover');
+  });
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
